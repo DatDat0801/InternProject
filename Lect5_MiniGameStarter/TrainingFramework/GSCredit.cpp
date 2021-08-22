2c7b454503ace2cc70f82f50e06240aa9bb44557
DatDat0801 <43485440+DatDat0801@users.noreply.github.com>
2021-08-22 19:55:01 +0700

    Change Logo, Menu and add Credit

---
+++ Lect5_MiniGameStarter/TrainingFramework/GSCredit.cpp
@@ -0,0 +1,112 @@
+#include "GSCredit.h"
+
+#include "Shader.h"
+#include "Texture.h"
+#include "Model.h"
+#include "Camera.h"
+#include "Font.h"
+#include "Sprite2D.h"
+#include "Sprite3D.h"
+#include "Text.h"
+#include "GameButton.h"
+
+GSCredit::GSCredit()
+{
+}
+
+
+GSCredit::~GSCredit()
+{
+}
+
+
+void GSCredit::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D.nfg");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("bg_main_menu.tga");
+
+	// background
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_background = std::make_shared<Sprite2D>(model, shader, texture);
+	m_background->Set2DPosition((float)Globals::screenWidth / 2, (float)Globals::screenHeight / 2);
+	m_background->SetSize(Globals::screenWidth, Globals::screenHeight);
+
+	// button clode
+	texture = ResourceManagers::GetInstance()->GetTexture("btn_close.tga");
+	std::shared_ptr<GameButton>  button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(Globals::screenWidth - 50, 50);
+	button->SetSize(50, 50);
+	button->SetOnClick([]() {
+		GameStateMachine::GetInstance()->PopState();
+		});
+	m_listButton.push_back(button);
+
+	// title
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("04B_30__.TTF");
+	m_title = std::make_shared< Text>(shader, font, "Nguyen Van Dat_PTIT", TextColor::RED, 1.0);
+	m_title->Set2DPosition(Globals::screenWidth / 2-200, Globals::screenHeight / 2-100);
+
+	// discription
+	m_discription = std::make_shared< Text>(shader, font, "Aim to shoot as many birds as possible.", TextColor::RED, 0.6);
+	m_discription->Set2DPosition(Globals::screenWidth / 2 - 220, Globals::screenHeight / 2-70);
+	m_discription2 = std::make_shared< Text>(shader, font, "If you hit a brick, you will lose points.", TextColor::RED, 0.6);
+	m_discription2->Set2DPosition(Globals::screenWidth / 2 - 220, Globals::screenHeight / 2 - 50);
+}
+
+void GSCredit::Exit()
+{
+}
+
+
+void GSCredit::Pause()
+{
+}
+
+void GSCredit::Resume()
+{
+}
+
+
+void GSCredit::HandleEvents()
+{
+}
+
+void GSCredit::HandleKeyEvents(int key, bool bIsPressed)
+{
+}
+
+void GSCredit::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto button : m_listButton)
+	{
+		if (button->HandleTouchEvents(x, y, bIsPressed))
+		{
+			break;
+		}
+	}
+}
+
+void GSCredit::HandleMouseMoveEvents(int x, int y)
+{
+}
+
+void GSCredit::Update(float deltaTime)
+{
+	for (auto it : m_listButton)
+	{
+		it->Update(deltaTime);
+	}
+}
+
+void GSCredit::Draw()
+{
+	m_background->Draw();
+	m_title->Draw();
+	m_discription->Draw();
+	m_discription2->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+}
